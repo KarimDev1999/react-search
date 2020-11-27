ff3e2e0c8209b65ae9f61d6ef2fa872985f1c7dd
Karim <cseed322@gmail.com>
2020-11-27 19:24:29 +0300

    Updates

---
+++ precache-manifest.528fea67112d0ce8f06cd482d0c3ef3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cbc779893c4ecabb6ddbbc811de28c0",
+    "url": "/react-search/index.html"
+  },
+  {
+    "revision": "99a078010217a5229358",
+    "url": "/react-search/static/css/main.7376f3e9.chunk.css"
+  },
+  {
+    "revision": "bdedec8f1aa654056784",
+    "url": "/react-search/static/js/2.c24cf5f8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-search/static/js/2.c24cf5f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99a078010217a5229358",
+    "url": "/react-search/static/js/main.f1d158b4.chunk.js"
+  },
+  {
+    "revision": "40ef68653a58bcc20c9c",
+    "url": "/react-search/static/js/runtime-main.a03e0726.js"
+  }
+]);
